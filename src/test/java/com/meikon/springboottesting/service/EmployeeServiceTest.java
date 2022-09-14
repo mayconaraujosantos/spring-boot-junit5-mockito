613c208f471f3c61d4d553a9520db20b90375e05
mayconaraujosantos <maycon.araujo.santos@gmail.com>
2022-09-13 21:34:23 -0300

    test: create unit test for deleted EmployeeService method

--- src/test/java/com/meikon/springboottesting/service/EmployeeServiceTest.java
+++ src/test/java/com/meikon/springboottesting/service/EmployeeServiceTest.java
@@ -3,8 +3,13 @@ package com.meikon.springboottesting.service;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.catchThrowableOfType;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import java.util.List;
 import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,7 +36,7 @@ class EmployeeServiceTest {
   @BeforeEach
   void setup() {
     employee = Employee.builder().id(1L).firstName("firstName").lastName("lastName")
-        .email("m@gmail.com").build();
+      .email("m@gmail.com").build();
   }
 
   @DisplayName("JUnit for save employee method")
@@ -53,10 +58,10 @@ class EmployeeServiceTest {
     given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.of(employee));
     // when
     ResourceNotFoundException thrown = catchThrowableOfType(
-        () -> employeeService.saveEmployee(employee), ResourceNotFoundException.class);
+      () -> employeeService.saveEmployee(employee), ResourceNotFoundException.class);
     // then
     assertThat(thrown).hasMessage("Employee already exist with given email: " + employee.getEmail())
-        .hasNoCause();
+      .hasNoCause();
   }
 
   @DisplayName("JUnit test for get all employees method")
@@ -64,7 +69,7 @@ class EmployeeServiceTest {
   void givenEmployeeList_whenGetAllEmployees_thenReturnEmployeesList() {
     // given
     var employeeBuild = Employee.builder().id(2L).firstName("firstName").lastName("lastName")
-        .email("email@gmail.com").build();
+      .email("email@gmail.com").build();
     given(employeeRepository.findAll()).willReturn(List.of(employee, employeeBuild));
     // when
     List<Employee> employeeList = employeeService.getAllEmployee();
@@ -98,4 +103,19 @@ class EmployeeServiceTest {
     assertThat(updatedEmployee.getEmail()).isEqualTo("ma@gmail.com");
     assertThat(updatedEmployee.getFirstName()).isEqualTo("firstNames");
   }
+
+  @DisplayName("JUnit test for deleteEmployee method")
+  @Test
+  void givenEmployeeUpdate_whenDeleteEmployee_thenNoThing() {
+    // given
+    long employeeId = 1L;
+    willDoNothing().given(employeeRepository).deleteById(1L);
+    // when
+    employeeService.deleteEmployee(employeeId);
+    // then
+    verify(employeeRepository, times(1)).deleteById(employeeId);
+  }
+  
+  
+  
 }
