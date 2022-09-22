b49d46d7e7efce878f88e942f3d6e9334890f0d9
Maycon Araujo <maycon.araujo.santos@gmail.com>
2022-09-22 14:06:45 -0300

    feat: ensure be list get by id employee

--- src/main/java/com/meikon/springboottesting/web/v1/EmployeeController.java
+++ src/main/java/com/meikon/springboottesting/web/v1/EmployeeController.java
@@ -4,7 +4,9 @@ import com.meikon.springboottesting.domain.entity.Employee;
 import com.meikon.springboottesting.domain.service.EmployeeService;
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +32,12 @@ public class EmployeeController {
   @GetMapping
   public List<Employee> getAllEmployee() {
     return employeeService.getAllEmployee();
+  }
+  
+  @GetMapping("/{id}")
+  public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") long employeeId) {
+    return employeeService.getEmployeeById(employeeId)
+      .map(ResponseEntity::ok)
+      .orElseGet(() -> ResponseEntity.notFound().build());
   }
 }
