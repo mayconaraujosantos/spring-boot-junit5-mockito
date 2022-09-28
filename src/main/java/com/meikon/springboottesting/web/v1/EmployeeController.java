877672f8645b1055d0ccf6b2e1980b6dcda18cc6
Maycon Araujo <maycon.araujo.santos@gmail.com>
2022-09-27 23:52:58 -0300

    feat: ensure employee service return 200 if deleted employee

--- src/main/java/com/meikon/springboottesting/web/v1/EmployeeController.java
+++ src/main/java/com/meikon/springboottesting/web/v1/EmployeeController.java
@@ -5,6 +5,7 @@ import com.meikon.springboottesting.domain.service.EmployeeService;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,5 +58,11 @@ public class EmployeeController {
 
       })
       .orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<String> deleteEmployee(@PathVariable("id") long employeeId) {
+    employeeService.deleteEmployee(employeeId);
+    return new ResponseEntity<>("Employee deleted successfully!.", HttpStatus.OK);
   }
 }
